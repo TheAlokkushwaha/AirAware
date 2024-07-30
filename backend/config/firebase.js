ae3fa060703dc83c5c315dd7798edd9cd43e6bea
Alok Kushwaha <103347563+TheAlokkushwaha@users.noreply.github.com>
2024-07-31 02:38:13 +0530

    Update firebase.js

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
 require('dotenv').config();
 // const serviceAccount = require('../flightstatus-9543e-firebase-adminsdk-qlp19-c9ea380d63.json');
-const serviceAccount=process.env.FIREBASE_SERVICE_ACCOUNT_KEY
+const serviceAccount=JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
