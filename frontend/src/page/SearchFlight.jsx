491cb9e1bd836a7e395df4e10507cfe8e1b74a6a
Alok Kushwaha <103347563+TheAlokkushwaha@users.noreply.github.com>
2024-07-31 02:51:07 +0530

    Update SearchFlight.jsx

--- frontend/src/page/SearchFlight.jsx
+++ frontend/src/page/SearchFlight.jsx
@@ -6,7 +6,7 @@ const SearchFlight = () => {
   const [flight, setFlight] = useState(null);
 
   const handleSearch = () => {
-    axios.get(`http://localhost:5000/api/flights/${flightId}`)
+    axios.get(`http://air-aware-r48m82gnl-thealokkushwahas-projects.vercel.app/api/flights/${flightId}`)
       .then(response => setFlight(response.data))
       .catch(error => console.error('Error fetching flight:', error));
   };
