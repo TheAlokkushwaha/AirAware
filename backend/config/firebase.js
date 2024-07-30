889bc344413f90968f90c3ffcba60ba176faa183
Alok Kushwaha <103347563+TheAlokkushwaha@users.noreply.github.com>
2024-07-31 02:23:23 +0530

    Update firebase.js

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,15 +1,6 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../flightstatus-9543e-firebase-adminsdk-qlp19-c9ea380d63.json');
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
-
-// const sendFirebaseMessage = (registrationToken, payload) => {
-//   return admin.messaging().sendToDevice(registrationToken, payload);
-// };
-
-// module.exports = sendFirebaseMessage;
+// const serviceAccount = require('../flightstatus-9543e-firebase-adminsdk-qlp19-c9ea380d63.json');
+const serviceAccount=process.env.FIREBASE_SERVICE_ACCOUNT_KEY
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
