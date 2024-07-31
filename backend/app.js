1c1600f25be967b1d29f49129aae178af1d36d32
Alok Kushwaha <103347563+TheAlokkushwaha@users.noreply.github.com>
2024-07-31 09:46:53 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,11 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // CORS Headers Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://air-aware-mel5.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 app.use(bodyParser.json());
 
